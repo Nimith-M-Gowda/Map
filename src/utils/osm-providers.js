9b6c1a05d292a6ea68938f70155e9b25f1e1cf39
Nimith-M-Gowda <nimithbbz10@gmail.com>
2021-04-08 15:41:52 +0530

    Add files via upload

---
+++ src/utils/osm-providers.js
@@ -0,0 +1,8 @@
+const osm = {
+  mapTiler: {
+    url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    attribution:
+      '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+  },
+};
+export default osm;
